45bf948baf065c296eead3b10c0e8cd0a6206bcf
Aleyda Davalos <aledavalos@Aleydas-MacBook-Pro.local>
2021-10-19 07:58:21 -0700

    updating files

--- Week3/Watchlist_Server_Project/server.js
+++ Week3/Watchlist_Server_Project/server.js
@@ -10,7 +10,7 @@ const seed = require ('./seed')
 const {db} = require('./db')
 const {User} = require ('./Models/Index')
 const {Show} = require('./Models/Index')
-const {id} = require('prelude-ls') //- wtf is this?
+const {id} = require('prelude-ls') 
 const { post } = require('request')
 const { templateSettings } = require('lodash')
 
@@ -33,21 +33,6 @@ app.get('/shows', async(req, res) => {
     res.json({allShows})
 })
 
-//allows us to get a single user id 
-// app.get("/user/:id", (req, res, next) => {
-//     let singleUser = "select * from user where id = ?"
-//     var id = [req.singleUser.id]
-//     db.get(singleUser, id, (err, row) => {
-//         if (err) {
-//           res.status(400).json({"error":err.message});
-//           return;
-//         }
-//         res.json({
-//             "message":"success",
-//             "user_data":row
-//         })
-//       });
-// });
 
 // route that will update a show by PK
 app.put('/show/:id', async( req, res) => {
@@ -68,6 +53,16 @@ app.post('/show/create', async(req, res) =>{
 
     res.send(`Show has been added ${name}`)
 })
+
+//route that will create a new user
+app.post('/user/create', async(req, res) =>{
+    let{ username} = require.body
+    await User.create({
+        username: username
+    })
+    res.send(`User ${username} has been added!`)
+})
+
 
 app.listen(PORT, () => {
     console.log(`Your server is now listening to port ${PORT}`)
